b7f9be6887f82f8b13d79ee4fa76a99000cac58e
AhadAli451 <ahadujjan23@gmail.com>
2024-09-02 21:48:28 -0700

    Update

---
+++ app.py
@@ -0,0 +1,5 @@
+print("hello world")
+print('''my name is ahad ali ujan''')
+print("ahad")
+name: str = "Ahad"
+[i for i in dir(name) if not i.startswith("_")]
---
+++ main.py
@@ -0,0 +1,11 @@
+name:str = "abdul ahad"
+print(name)
+
+#object.method()
+#object.attribute
+
+print( name.capitalize()) 
+print( name.casefold() )
+print( name.upper() )
+print( name.lower() )
+print( name.title() )
